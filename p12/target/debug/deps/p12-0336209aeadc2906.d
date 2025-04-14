96bc818a89f30b2bc2f9d3c64b16bf6b029dd644
Marco Palumbi <Marco.Palumbi@tii.ae>
2025-04-14 09:20:32 +0000

    p12 projec

---
+++ p12/target/debug/deps/p12-0336209aeadc2906.d
@@ -0,0 +1,5 @@
+/home/marco/Shared/Rust_training/workshop2025/new-workshop/p12/target/debug/deps/libp12-0336209aeadc2906.rmeta: src/main.rs
+
+/home/marco/Shared/Rust_training/workshop2025/new-workshop/p12/target/debug/deps/p12-0336209aeadc2906.d: src/main.rs
+
+src/main.rs:
