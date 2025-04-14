bfb6994f247534eb2f29039c4f1924dd61ad725f
Marco Palumbi <Marco.Palumbi@tii.ae>
2025-04-14 10:07:26 +0000

    remove target directory

--- p12/target/debug/deps/p12-a73b5e227495ccb3.d
+++
@@ -1,5 +0,0 @@
-/home/marco/Shared/Rust_training/workshop2025/new-workshop/p12/target/debug/deps/libp12-a73b5e227495ccb3.rmeta: src/main.rs
-
-/home/marco/Shared/Rust_training/workshop2025/new-workshop/p12/target/debug/deps/p12-a73b5e227495ccb3.d: src/main.rs
-
-src/main.rs:
